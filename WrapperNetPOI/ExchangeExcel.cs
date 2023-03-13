e41b146b2904f0485b4f655a94025932f14a82d5
Александр М <sashamsuper@yandex.ru>
2023-03-13 19:53:56 +0300

    Новая ветьв master OLD

--- WrapperNetPOI/ExchangeExcel.cs
+++ WrapperNetPOI/ExchangeExcel.cs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==========================================================================*/
 
+
 using NPOI.POIFS.Crypt;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
